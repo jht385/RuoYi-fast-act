44f9342af03a73c8dfea470c1f75419135354374
jht385 <251064029@qq.com>
2025-05-09 15:36:10 +0800

    更新到 06ddc6d6

--- src/main/java/com/ruoyi/framework/config/ShiroConfig.java
+++ src/main/java/com/ruoyi/framework/config/ShiroConfig.java
@@ -155,7 +155,7 @@ public class ShiroConfig
     @Bean
     public EhCacheManager getEhCacheManager()
     {
-        net.sf.ehcache.CacheManager cacheManager = net.sf.ehcache.CacheManager.getCacheManager("ruoyi");
+        net.sf.ehcache.CacheManager cacheManager = net.sf.ehcache.CacheManager.getCacheManager("ry-fast-act");
         EhCacheManager em = new EhCacheManager();
         if (StringUtils.isNull(cacheManager))
         {
